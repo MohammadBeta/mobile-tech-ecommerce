7c6f1c2a426516252fedbbcb6000e94e531dac06
Mohammad Beta <swiftbooksjob@hotmail.com>
2024-06-28 11:58:07 +0300

    add constants colors and styles

---
+++ mobile_tech_ecommerce_application/lib/core/constants/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color black = Color(0xff0C0C0C);
+  static const Color white = Color(0xffFFFFFF);
+
+  static const Color primary = Color(0xff0C68F4);
+  static const Color primary500 = Color(0xff063A88);
+  static const Color primary400 = Color(0xff0951BE);
+
+  static const Color secondary = Color(0xffF45E0C);
+
+  static const Color gray444444 = Color(0xff444444);
+  static const Color gray2D2D2D = Color(0xff2D2D2D);
+  static const Color gray505050 = Color(0xff505050);
+  static const Color grayB4B4B4 = Color(0xffB4B4B4);
+  static const Color grayEDEDED = Color(0xffEDEDED);
+  static const Color gray9E9E9E = Color(0xff9E9E9E);
+  static const Color gray717171 = Color(0xff717171);
+
+  static const Color brown91610F = Color(0xff91610F);
+
+  static const Color error = Color(0xffC91433);
+}
---
+++ mobile_tech_ecommerce_application/lib/core/constants/app_styles.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'app_colors.dart';
+
+abstract class AppTextStyles {
+  static TextStyle styleInterLight12 = const TextStyle(
+      fontSize: 12, fontWeight: FontWeight.w300, color: AppColors.black);
+
+  static TextStyle styleInterLight14 = const TextStyle(
+      fontSize: 14, fontWeight: FontWeight.w300, color: AppColors.black);
+  static TextStyle styleInterLight16 = const TextStyle(
+      fontSize: 16, fontWeight: FontWeight.w300, color: AppColors.black);
+  static TextStyle styleInterMedium10 = const TextStyle(
+      fontSize: 10, fontWeight: FontWeight.w500, color: AppColors.black);
+  static TextStyle styleInterMedium12 = const TextStyle(
+      fontSize: 12, fontWeight: FontWeight.w500, color: AppColors.black);
+  static TextStyle styleInterMedium14 = const TextStyle(
+      fontSize: 14, fontWeight: FontWeight.w500, color: AppColors.black);
+  static TextStyle styleInterMedium16 = const TextStyle(
+      fontSize: 16, fontWeight: FontWeight.w500, color: AppColors.black);
+
+  static TextStyle styleInterMedium24 = const TextStyle(
+      fontSize: 24, fontWeight: FontWeight.w500, color: AppColors.primary400);
+  static TextStyle styleInterRegular10 = const TextStyle(
+      fontSize: 10, fontWeight: FontWeight.w400, color: AppColors.black);
+  static TextStyle styleInterRegular12 = const TextStyle(
+      fontSize: 12, fontWeight: FontWeight.w400, color: AppColors.gray717171);
+  static TextStyle styleInterRegular14 = const TextStyle(
+      fontSize: 14, fontWeight: FontWeight.w400, color: AppColors.black);
+
+  static TextStyle styleRobotoRegular23 = TextStyle(
+      fontSize: 23,
+      fontWeight: FontWeight.w400,
+      fontFamily: GoogleFonts.roboto().fontFamily,
+      color: AppColors.black);
+
+  static TextStyle styleRobotoBold10 = TextStyle(
+      fontSize: 10,
+      fontWeight: FontWeight.w700,
+      fontFamily: GoogleFonts.roboto().fontFamily,
+      color: AppColors.gray444444);
+}
